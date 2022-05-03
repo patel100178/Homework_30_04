9e0c1e199187b117997cebe44aee5fd18a2ea597
Payal <Patel100178@gmail.com>
2022-05-03 21:38:58 +0100

    two times table

---
+++ timetables/src/TwoTimesTable.java
@@ -0,0 +1,11 @@
+public class TwoTimesTable {
+    public static void main(String[] args) {
+
+        int a= 2;
+        for(int i =1; i<=12; i++){
+            System.out.println(a+"x " +i+ " = "+ i*a);
+        }
+    }
+
+
+}
